27ea56a446781cecce12835f224864d6b8c62a33
jstalinko <alinkodotid@gmail.com>
2026-05-14 10:57:56 +0700

    fix: some small details II

--- resources/js/pages/Member/register.vue
+++ resources/js/pages/Member/register.vue
@@ -269,11 +269,11 @@
                   <label class="field-label">Status Keanggotaan <span class="text-red-500">*</span></label>
                   <select v-model="form.status_keanggotaan" :class="['field-input', form.errors.status_keanggotaan ? 'border-red-400 bg-red-50' : '']">
                     <option value="" disabled>Pilih status</option>
-                    <option>SS DIPONEGORO</option>
-                    <option>LIFE MEMBER</option>
-                    <option>HONORARY</option>
-                    <option>VIRGIN</option>
-                    <option>PROSPECT</option>
+                    <option value="SS DIPONEGORO">SS DIPONEGORO</option>
+                    <option value="LIFE MEMBER">LIFE MEMBER</option>
+                    <option value="HONORARY">HONORARY</option>
+                    <option value="VIRGIN">VIRGIN</option>
+                    <option value="PROSPECT">PROSPECT</option>
                   </select>
                   <p v-if="form.errors.status_keanggotaan" class="field-error">{{ form.errors.status_keanggotaan }}</p>
                 </div>
